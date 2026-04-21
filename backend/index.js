5b2035192bdb65869d2ec054f92d53192e14da93
mirzazohaib56 <sajjadzohaib929@gmail.com>
2026-04-21 19:46:21 +0500

    first commit

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,28 @@ console.log(process.env.MONGO_DB_URL)
 const app = express();
 
 
-await connectDB();
-
 app.use(cors({
     origin: '*'
 }))
 
-
-
 app.use('/auth', authRoutes)
+
+const startServer = async () => {
+  try {
+    console.log("DB URL:", process.env.MONGO_DB_URL);
+
+    await connectDB();
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`🚀 App is running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Server failed to start:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
